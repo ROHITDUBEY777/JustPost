e504f4dc871a802e0216df3f5b010e7117aff3fe
Rohit Dubey <166648226+ROHITDUBEY777@users.noreply.github.com>
2026-02-19 22:35:30 +0530

    Change socket connection URL to production server

--- roomlink-share-direct/src/socket.js
+++ roomlink-share-direct/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://justpost-151e.onrender.com", {
   transports: ["websocket"]
 });
